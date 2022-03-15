0bc7dba418e6422b40c4987194233f5db8a6a900
Sandesh Poudel <sandesh100poudel@gmail.com>
2022-03-14 21:27:41 -0700

    important authentication

---
+++ HASING-basic.js
@@ -0,0 +1,92 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
+const md5 = require("md5");
+
+
+
+const app=express();
+console.log(process.env.SECRETE);
+
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser:true} ,()=>{
+    console.log("connected to mongodb");
+});
+
+const userSchema = new mongoose.Schema({
+    "email":String,
+    "password":String
+});
+
+
+
+
+const User=new mongoose.model("User", userSchema);
+
+
+
+app.get("/", (req,res)=>{
+    res.render("home");
+})
+
+app.get("/register", (req,res)=>{
+    res.render("register");
+});
+
+app.get("/login", (req,res)=>{
+    res.render("login");
+});
+
+
+
+
+
+
+
+app.post("/register",(req, res)=>{
+    const newUser = new User({
+        email:req.body.username,
+        password:md5(req.body.password)
+    });
+
+    newUser.save((err)=>{
+        if(err){
+            console.log(error)
+        }else{
+            res.render("secrets");
+        }
+    })
+});
+
+
+app.post("/login", (req,res)=>{
+    const username=req.body.username;
+    const password=md5(req.body.password);
+
+    User.findOne({email:username},(err,foundUser)=>{
+        if(err){
+            console.log(err);
+        
+        }else{
+            if (foundUser){
+                if(foundUser.password===password){
+                    res.render("secrets");
+                }else{
+                    res.send("incorrect password of email , try once again");
+                }
+            }
+        }
+    });
+});
+
+
+app.listen(3000,()=>{
+    console.log("server is running");
+})
